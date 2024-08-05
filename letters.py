d38b518dbd4901fa7fbad4d889e1b7d31fd4d1ce
Grattapitone <116570758+Grattapitone@users.noreply.github.com>
2024-08-05 13:12:01 +0200

    added "A"

--- letters.py
+++ letters.py
@@ -1,6 +1,12 @@
 import turtle as t
 
 def letters():
-  pass
+  if letter == "A":
+      t.goto(25,100)
+      t.goto(50,0)
+      t.up()
+      t.goto(13,50)
+      t.down()
+      t.forward(25)
 
 t.done()
