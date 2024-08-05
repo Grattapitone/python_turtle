ea3e256561f0a2011ebb68e0d726bac0e5a85852
Grattapitone <116570758+Grattapitone@users.noreply.github.com>
2024-08-05 15:30:46 +0200

    added letters B and P and the ellipse function
    special thanks to Devansu Yadav for the ellipse code

--- letters.py
+++ letters.py
@@ -1,4 +1,31 @@
 import turtle as t
+import math
+
+t.shape("turtle")
+t.fillcolor("blue")
+
+def ellipse(a, b, h=None, k=None, angle=None, angle_unit=None):
+    if h is None:
+        h = 0
+    if k is None:
+        k = 0
+    # Angle unit can be degree or radian
+    if angle is None:
+        angle = 360
+        converted_angle = angle * 0.875
+    if angle_unit == "d" or angle_unit is None:
+        converted_angle = angle * 0.875
+    # We are multiplying by 0.875 because for making a complete ellipse we are plotting 315 pts according
+    # to our parametric angle value
+    elif angle_unit == "r":
+        converted_angle = (angle * 0.875 * (180/math.pi))
+    # Converting radian to degrees.
+    for i in range(int(converted_angle) + 1):
+        if i == 0:
+            t.up()
+        else:
+            t.down()
+        t.setposition((h+a*math.sin(i/50)), (k + b * math.cos(i/50)))
 
 def draw_letter(x,y,width,height,letter):
     t.up()
@@ -11,6 +38,13 @@ def draw_letter(x,y,width,height,letter):
         t.goto(x + width / 4, y + height / 2)
         t.down()
         t.forward(width / 2)
+    elif letter == "B" or letter == "P":
+        t.lt(90)
+        t.forward(height)
+        t.rt(90)
+        ellipse(width,height / 4,x,y + height * (3/4),180)
+        if letter == "B":
+            ellipse(width,height / 4,x,y + height / 4,180)
     elif letter == " ":
         t.up()
         t.forward(width)
@@ -25,7 +59,7 @@ def draw_letter(x,y,width,height,letter):
         t.forward(height)
         t.lt(90)
 
-def draw_word(x,y,word):
+def draw_word(word,y,x = "centered"):
     t.seth(0)
     if x == "centered":
         length = len(word) * 100 + (len(word) - 1) * 25
@@ -38,7 +72,7 @@ def draw_word(x,y,word):
             draw_letter(x,y,100,200,character)
             x += 125
 
-draw_word("centered",0,"A B")
+draw_word("ABP Z",0)
 t.hideturtle()
 
 t.done()
