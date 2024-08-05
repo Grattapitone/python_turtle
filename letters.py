5f3997dc91bcaf92f3fa162494da28e83cfc8c73
Grattapitone <116570758+Grattapitone@users.noreply.github.com>
2024-08-05 19:09:26 +0200

    added "CDEFGHI"

--- letters.py
+++ letters.py
@@ -4,7 +4,7 @@ import math
 t.shape("turtle")
 t.fillcolor("blue")
 
-def ellipse(a, b, h=None, k=None, angle=None, angle_unit=None):
+def ellipse(a, b, h=None, k=None, angle=None, negate = 1, angle_unit=None):
     if h is None:
         h = 0
     if k is None:
@@ -25,9 +25,10 @@ def ellipse(a, b, h=None, k=None, angle=None, angle_unit=None):
             t.up()
         else:
             t.down()
-        t.setposition((h+a*math.sin(i/50)), (k + b * math.cos(i/50)))
+        t.setposition(negate*(h+a*math.sin(i/50)), (k + b * math.cos(i/50)))
 
 def draw_letter(x,y,width,height,letter):
+    t.seth(0)
     t.up()
     t.goto(x,y)
     t.down()
@@ -45,6 +46,47 @@ def draw_letter(x,y,width,height,letter):
         ellipse(width,height / 4,x,y + height * (3/4),180)
         if letter == "B":
             ellipse(width,height / 4,x,y + height / 4,180)
+    elif letter == "C":
+        ellipse(width / 2, height / 2, -x - width / 2, y + height / 2,225,-1)
+    elif letter == "D":
+        t.lt(90)
+        t.forward(height)
+        t.rt(90)
+        ellipse(width,height / 2,x,y + height / 2,180)
+    elif letter == "E" or letter == "F":
+        if letter == "E":
+            t.forward(width)
+            t.backward(width)
+        t.lt(90)
+        t.forward(height / 2)
+        t.rt(90)
+        t.forward(width)
+        t.backward(width)
+        t.lt(90)
+        t.forward(height / 2)
+        t.rt(90)
+        t.forward(width)
+        t.backward(width)
+    elif letter == "G":
+        ellipse(width / 2, height / 2, -x - width / 2, y + height / 2,270,-1)
+        t.backward(width / 2)
+    elif letter == "H":
+        t.lt(90)
+        t.forward(height)
+        t.backward(height / 2)
+        t.rt(90)
+        t.forward(width)
+        t.rt(90)
+        t.backward(height / 2)
+        t.forward(height)
+    elif letter == "I":
+        t.forward(width)
+        t.backward(width / 2)
+        t.lt(90)
+        t.forward(height)
+        t.rt(90)
+        t.backward(width / 2)
+        t.forward(width)
     elif letter == " ":
         t.up()
         t.forward(width)
@@ -71,8 +113,9 @@ def draw_word(word,y,x = "centered"):
         for character in word:
             draw_letter(x,y,100,200,character)
             x += 125
-
-draw_word("ABP Z",0)
+t.speed("fastest")
+draw_word("ABCDEF",100)
+draw_word("GHIP Z",-150)
 t.hideturtle()
 
 t.done()
