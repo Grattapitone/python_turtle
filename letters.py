c74ccb3a90add234305b30f1dba330e06658ef9c
Grattapitone <116570758+Grattapitone@users.noreply.github.com>
2024-08-05 13:16:53 +0200

    Add files via upload

--- letters.py
+++ letters.py
@@ -1,12 +1,14 @@
 import turtle as t
 
-def letters():
+def letters(letter):
   if letter == "A":
       t.goto(25,100)
       t.goto(50,0)
       t.up()
-      t.goto(13,50)
+      t.goto(12,50)
       t.down()
       t.forward(25)
+
+letters("A")
 
 t.done()
